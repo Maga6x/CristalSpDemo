586f860ee7a893adcf8698728bb047c47a6034dd
user <magzhankabi@gmail.com>
2025-09-10 11:03:20 +0500

    Spring Security+JWT new3

--- src/main/resources/db/changelog/changes/10092025-1230-insert-table.sql
+++ src/main/resources/db/changelog/changes/10092025-1230-insert-table.sql
@@ -13,22 +13,29 @@ VALUES('maga',   '$2a$10$jXH8/ecxGeRTHQx21OHwV.XcKm9b3HopSmNwqmdFWi6tBPXuMo46K',
 ON CONFLICT (username) DO NOTHING;
 
 
--- changeset alisher:4
+-- changeset alisher:3
 INSERT INTO user_roles(user_id, role_id)
 VALUES (1,1),
        (2,2);
 
--- changeset alisher:3
-INSERT INTO user_roles (user_id, role_id)
-SELECT u.id, r.id
-FROM users u JOIN roles r ON r.name = 'USER'
-WHERE u.username = 'maga'
-ON CONFLICT DO NOTHING;
-
 -- changeset alisher:4
 INSERT INTO user_roles (user_id, role_id)
 SELECT u.id, r.id
-FROM users u JOIN roles r ON r.name = 'ADMIN'
+FROM users u JOIN roles r ON r.name = 'ROLE_USER'
+WHERE u.username = 'maga'
+ON CONFLICT DO NOTHING;
+
+-- changeset alisher:5
+INSERT INTO user_roles (user_id, role_id)
+SELECT u.id, r.id
+FROM users u JOIN roles r ON r.name = 'ROLE_ADMIN'
 WHERE u.username = 'darhan'
 ON CONFLICT DO NOTHING;
+
+-- changeset alisher:6
+UPDATE databasechangelog
+SET md5sum = NULL
+WHERE filename = 'db/changelog/changes/10092025-1230-insert-table.sql'
+  AND author   = 'alisher'
+  AND id IN ('3','4');
 
