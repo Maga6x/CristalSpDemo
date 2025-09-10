8f01759664ea10535f0e24a548cb665d7e3be70f
user <magzhankabi@gmail.com>
2025-09-10 10:48:40 +0500

    Spring Security+JWT new2

--- src/main/resources/db/changelog/changes/10092025-1230-insert-table.sql
+++ src/main/resources/db/changelog/changes/10092025-1230-insert-table.sql
@@ -2,14 +2,14 @@
 
 -- changeset alisher:1
 INSERT INTO roles (name, description)
-VALUES('USER', 'обычный пользователь'),
-      ('ADMIN', 'администратор')
+VALUES('ROLE_USER', 'обычный пользователь'),
+      ('ROLE_ADMIN', 'администратор')
 ON CONFLICT (name) DO NOTHING;
 
 -- changeset alisher:2
 INSERT INTO users (username, password_hash, full_name, age, gender)
-VALUES('maga',   '$2a$10$zhNbw/bayIkGi09xc1nKU0a79AqpMYx07VJl3Utq4re2hGeOE2WRu', 'maga',   19, 'male'),
-      ('darhan', '$2a$10$zhNbw/bayIkGi09xc1nKU0a79AqpMYx07VJl3Utq4re2hGeOE2WRu', 'darhan', 20, 'male')
+VALUES('maga',   '$2a$10$jXH8/ecxGeRTHQx21OHwV.XcKm9b3HopSmNwqmdFWi6tBPXuMo46K', 'maga',   19, 'male'),
+      ('darhan', '$2a$10$jXH8/ecxGeRTHQx21OHwV.XcKm9b3HopSmNwqmdFWi6tBPXuMo46K', 'darhan', 20, 'male')
 ON CONFLICT (username) DO NOTHING;
 
 -- changeset alisher:3
